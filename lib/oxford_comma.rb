a1e769d74a62e2b8490665bc9f7f84e0023499f0
Camryn <camrynpatterson@yahoo.com>
2020-11-18 15:57:05 -0600

    commit tree 420cde3b500f962f0161e2bbd8f49020c6bebd43

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+if array.length == 1 
+array.join('')
+    
+    elsif array.length == 2 
+      array.join(' and ')
+      elsif array.length >= 3 
+        array[-1] = "and #{array[-1]}"
+        array.join(', ')
+      else 
+        nil 
+      end 
+  end   
+    
+  oxford_comma(["boxer"])
+  oxford_comma(["boxer", "lab"])
+  oxford_comma(["boxer", "lab", "great dane"])
+  oxford_comma(['boxer', 'lab', 'great dane', 'pug', 'yorkie'])
 
-end
+
